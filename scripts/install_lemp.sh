fef8202b84c4677ddb76d3f69fce609dd1f6dadb
samuel <samuel@gmail.com>
2024-03-13 08:43:36 +0000

    correcion

--- scripts/install_lemp.sh
+++ scripts/install_lemp.sh
@@ -18,11 +18,6 @@ apt install mysql-server -y
 # Instalacion de paquetes: paquete php-fpm y php-mysql
 apt install php-fpm php-mysql -y
 
-#-------------------#
-# php-fpm permite mejorar el consumo de memoria con mucho tráfico, este se ejecutará como un servicio independiente de Nginx.
-# php-mysql permite a php interar con el sistema gestor de base de datos.
-#-------------------#
-
 # Copiamos el contenido de default de default a /etc/nginx/sites-available/
 cp ../conf/default /etc/nginx/sites-available/default
 
@@ -31,6 +26,3 @@ systemctl restart nginx
 
 # Damos permisos al usuario de nginx
 chown -R www-data:www-data /var/www/html
-
-# Copiamos el contenido de php a html para comprobar que funciona.
-cp ../php/info.php /var/www/html
